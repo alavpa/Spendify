bbc2cbc361d175ab3cb84b8320f652565bd11b49
Alejandro Avila <alejandro.avila@axa-groupsolutions.com>
2017-04-07 21:49:46 +0200

    remove copyright

--- app/src/main/java/com/alavpa/spendify/ui/receiver/alarm/AlarmPresenter.java
+++ app/src/main/java/com/alavpa/spendify/ui/receiver/alarm/AlarmPresenter.java
@@ -1,20 +1,5 @@
 package com.alavpa.spendify.ui.receiver.alarm;
 
-/*
- * Copyright (c) 2017 AXA Group Solutions.
- *
- * Licensed under the AXA Group Solutions License (the "License"); you
- * may not use this file except in compliance with the License.
- * A copy of the License can be found in the LICENSE.TXT file distributed
- * together with this file.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import com.alavpa.spendify.data.preferences.PrefsDatasource;
 import com.alavpa.spendify.domain.DateUtils;
 import com.alavpa.spendify.domain.model.AlarmAmount;
